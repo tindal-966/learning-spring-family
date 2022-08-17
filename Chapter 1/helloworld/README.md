12e743e63dff8fab07d4b9b3ced3e1ba946136f1
tindal-966 <qinghuang1995@gmail.com>
2022-08-17 23:13:19 +0800

    docs: C01-helloworld

---
+++ Chapter 1/helloworld/README.md
@@ -0,0 +1,5 @@
+# Hello World
+- *Hello, world!* 项目
+- pom-without-parent.xml 演示的是在不能使用 spring-boot-starter-parent 作为 Parent POM 时还需要使用 SpringBoot 的配置
+    
+    参考 [Using Spring Boot without the Parent POM](https://docs.spring.io/spring-boot/docs/current/maven-plugin/reference/htmlsingle/#using.import)
--- Chapter 1/helloworld/src/test/java/geektime/demo/HelloWorldApplicationTests.java
+++ Chapter 1/helloworld/src/test/java/geektime/demo/HelloWorldApplicationTests.java
@@ -1,17 +1,26 @@
 package geektime.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 参考：https://docs.spring.io/spring-boot/docs/2.1.1.RELEASE/reference/html/boot-features-testing.html#boot-features-testing-spring-boot-applications-testing-with-running-server
+ */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloWorldApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void testHelloController() {
+		String body = this.restTemplate.getForObject("/hello", String.class);
+		Assertions.assertThat(body).isEqualTo("Hello World!");
 	}
-
 }
 
