e09e6290046ab88fd21ca891eac752443c47915a
tindal-966 <qinghuang1995@gmail.com>
2022-08-20 15:21:57 +0800

    docs: C13-circuit-break-demo

---
+++ Chapter 13/circuit-break-demo/README.md
@@ -0,0 +1,2 @@
+# Circuit Break Demo
+AOP 实现类似 Hystrix 的断路保护
--- Chapter 13/circuit-break-demo/src/main/java/geektime/spring/springbucks/customer/support/CircuitBreakerAspect.java
+++ Chapter 13/circuit-break-demo/src/main/java/geektime/spring/springbucks/customer/support/CircuitBreakerAspect.java
@@ -15,32 +15,33 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class CircuitBreakerAspect {
     private static final Integer THRESHOLD = 3;
-    private Map<String, AtomicInteger> counter = new ConcurrentHashMap<>();
-    private Map<String, AtomicInteger> breakCounter = new ConcurrentHashMap<>();
+    private Map<String, AtomicInteger> errorCounter = new ConcurrentHashMap<>(); // 失败次数
+    private Map<String, AtomicInteger> breakCounter = new ConcurrentHashMap<>(); // 断路保护次数
 
-    @Around("execution(* geektime.spring.springbucks.customer.integration..*(..))")
+    @Around("execution(* geektime.spring.springbucks.customer.integration..*(..))") // 拦截 Feign 的接口
     public Object doWithCircuitBreaker(ProceedingJoinPoint pjp) throws Throwable {
-        String signature = pjp.getSignature().toLongString();
+        String signature = pjp.getSignature().toLongString(); // 方法签名
         log.info("Invoke {}", signature);
+
         Object retVal;
         try {
-            if (counter.containsKey(signature)) {
-                if (counter.get(signature).get() > THRESHOLD &&
-                        breakCounter.get(signature).get() < THRESHOLD) {
+            if (errorCounter.containsKey(signature)) {
+                if (errorCounter.get(signature).get() > THRESHOLD &&
+                        breakCounter.get(signature).get() < THRESHOLD) { // 当 breakCounter 大于默认值的话会尝试自动恢复
                     log.warn("Circuit breaker return null, break {} times.",
                             breakCounter.get(signature).incrementAndGet());
                     return null;
                 }
             } else {
-                counter.put(signature, new AtomicInteger(0));
+                errorCounter.put(signature, new AtomicInteger(0));
                 breakCounter.put(signature, new AtomicInteger(0));
             }
             retVal = pjp.proceed();
-            counter.get(signature).set(0);
+            errorCounter.get(signature).set(0);
             breakCounter.get(signature).set(0);
         } catch (Throwable t) {
             log.warn("Circuit breaker counter: {}, Throwable {}",
-                    counter.get(signature).incrementAndGet(), t.getMessage());
+                    errorCounter.get(signature).incrementAndGet(), t.getMessage());
             breakCounter.get(signature).set(0);
             throw t;
         }
