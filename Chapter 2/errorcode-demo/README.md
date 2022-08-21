3e52dac154d954a8980907c6ba1cf5d3fd5d7625
tindal-966 <qinghuang1995@gmail.com>
2022-08-22 04:38:25 +0800

    docs: Chapter 2\errorcode-demo

---
+++ Chapter 2/errorcode-demo/README.md
@@ -0,0 +1,2 @@
+# ErrorCode Demo
+自定义 JDBC error code，主要参考 sql-error-codes.xml 文件
--- Chapter 2/errorcode-demo/src/main/java/geektime/spring/data/errorcodedemo/CustomDuplicatedKeyException.java
+++ Chapter 2/errorcode-demo/src/main/java/geektime/spring/data/errorcodedemo/CustomDuplicatedKeyException.java
@@ -2,6 +2,9 @@ package geektime.spring.data.errorcodedemo;
 
 import org.springframework.dao.DuplicateKeyException;
 
+/**
+ * 自定义键重复异常
+ */
 public class CustomDuplicatedKeyException extends DuplicateKeyException {
     public CustomDuplicatedKeyException(String msg) {
         super(msg);
