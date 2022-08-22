2b9935f45759485890dcabe5f36e8e50dc7fa5bd
tindal-966 <qinghuang1995@gmail.com>
2022-08-23 05:06:52 +0800

    docs: Chapter 5/simple-reactor-demo

---
+++ Chapter 5/simple-reactor-demo/README.md
@@ -0,0 +1,2 @@
+# Simple Reactor Demo
+Simple Reactor Demo
--- Chapter 5/simple-reactor-demo/src/main/java/geektime/spring/reactor/simple/SimpleReactorDemoApplication.java
+++ Chapter 5/simple-reactor-demo/src/main/java/geektime/spring/reactor/simple/SimpleReactorDemoApplication.java
@@ -19,22 +19,22 @@ public class SimpleReactorDemoApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		Flux.range(1, 6)
+		Flux.range(1, 6) // 创建 1~6 的序列
 				.doOnRequest(n -> log.info("Request {} number", n)) // 注意顺序造成的区别
-//				.publishOn(Schedulers.elastic())
+//				.publishOn(Schedulers.elastic()) // 解开注释的话：会在 elastic 线程池中执行下面内容
 				.doOnComplete(() -> log.info("Publisher COMPLETE 1"))
 				.map(i -> {
-					log.info("Publish {}, {}", Thread.currentThread(), i);
-					return 10 / (i - 3);
-//					return i;
+					log.info("Publish {}, {}", Thread.currentThread(), i); // 演示序列中的每个值在哪个线程执行（都在主线程）
+//					return 10 / (i - 3);
+					return i;
 				})
 				.doOnComplete(() -> log.info("Publisher COMPLETE 2"))
 //				.subscribeOn(Schedulers.single())
-//				.onErrorResume(e -> {
+//				.onErrorResume(e -> { // 解开注释（配合 return 10 / (i - 3);）的话：publish 3 能正常 subscribe 出 -1 之前显示日志
 //					log.error("Exception {}", e.toString());
-//					return Mono.just(-1);
+//					return Mono.just(-2); // 即使同时解开 .onErrorReturn(-1) 也会返回 -2
 //				})
-//				.onErrorReturn(-1)
+//				.onErrorReturn(-1) // 解开注释（配合 return 10 / (i - 3);）的话：publish 3 能正常 subscribe 出 -1，但不会往下执行
 				.subscribe(i -> log.info("Subscribe {}: {}", Thread.currentThread(), i),
 						e -> log.error("error {}", e.toString()),
 						() -> log.info("Subscriber COMPLETE")//,
